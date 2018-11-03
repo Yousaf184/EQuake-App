e540a9d1e8fe8e134bd44e8a330b4f8af6051179
Yousaf Khan <khanyousaf184@gmail.com>
2018-11-04 01:25:11 +0500

    added ViewModel and LiveData for loading and managing data

---
+++ app/src/main/java/com/example/yousafkhan/equake/EarthQuakeVModel.java
@@ -0,0 +1,157 @@
+package com.example.yousafkhan.equake;
+
+import android.arch.lifecycle.MutableLiveData;
+import android.arch.lifecycle.ViewModel;
+import android.os.AsyncTask;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.util.ArrayList;
+
+public class EarthQuakeVModel extends ViewModel {
+
+    private static MutableLiveData<ArrayList<EarthQuake>> earthQuakesInfoList;
+    private static final String apiURL =
+            "https://earthquake.usgs.gov/fdsnws/event/1/query?format=geojson&starttime=2018-01-01&limit=30";
+
+    public MutableLiveData<ArrayList<EarthQuake>> getDataList() {
+
+        if(earthQuakesInfoList == null) {
+            // load data only for the first time
+            loadData();
+            earthQuakesInfoList = new MutableLiveData<>();
+        }
+
+        return earthQuakesInfoList;
+    }
+
+    public EarthQuake getItemAtPosition(int position) {
+        return earthQuakesInfoList.getValue().get(position);
+    }
+
+    private void loadData() {
+        new ApiRequest().execute(apiURL);
+    }
+
+    private void parseJSON(String jsonString) {
+
+        ArrayList<EarthQuake> list = new ArrayList<>();
+
+        double magnitude = 0;
+        String location = "";
+        long time = 0;
+        String detailUrl = "";
+
+        try {
+            // convert JSON string in to JSON object
+            JSONObject jsonObj = new JSONObject(jsonString);
+            // get the features array from the JSON object
+            JSONArray jsonArr = jsonObj.getJSONArray("features");
+
+            // iterate over the features array and extract the required information
+            for (int i=0; i<jsonArr.length(); i++) {
+                // get the JSON object on the current index of the features array
+                // instead of declaring new JSONObject, already declared jsonObj is reused here
+                jsonObj = jsonArr.getJSONObject(i);
+
+                // get the properties object from the current JSON object
+                jsonObj = jsonObj.getJSONObject("properties");
+
+                // get the required information in local variables
+                magnitude = Double.parseDouble(jsonObj.getString("mag"));
+                location = jsonObj.getString("place");
+                time = Long.parseLong(jsonObj.getString("time"));
+                detailUrl = jsonObj.getString("url");
+
+                // create EarthQuake object
+                EarthQuake eq = new EarthQuake(magnitude, location, time, detailUrl);
+
+                // add the newly created EarthQuake object to array list
+                list.add(eq);
+            }
+
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
+
+        // update live data
+        earthQuakesInfoList.postValue(list);
+    }
+
+    // async task class
+    private static class ApiRequest extends AsyncTask<String, Void, InputStream> {
+
+        private EarthQuakeVModel eqModel = new EarthQuakeVModel();
+
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+        }
+
+        @Override
+        protected InputStream doInBackground(String... strings) {
+
+            String apiURL = strings[0];
+            URL url = null;
+            HttpURLConnection httpConn = null;
+            InputStream inputStream = null;
+
+            try {
+                url = new URL(apiURL);
+                httpConn = (HttpURLConnection) url.openConnection();
+                httpConn.setRequestMethod("GET");
+                inputStream = httpConn.getInputStream();
+
+            } catch (Exception ex) {
+                ex.printStackTrace();
+            } finally {
+                if(httpConn != null) {
+                    httpConn.disconnect();
+                }
+            }
+
+            return inputStream;
+        }
+
+        @Override
+        protected void onProgressUpdate(Void... values) {
+            super.onProgressUpdate(values);
+        }
+
+        @Override
+        protected void onPostExecute(InputStream ins) {
+
+            if(ins != null) {
+
+                String line = "";
+                StringBuilder strBuilder = new StringBuilder();
+
+                InputStreamReader inputStreamReader = new InputStreamReader(ins);
+                BufferedReader bufferedReader = new BufferedReader(inputStreamReader);
+
+                try {
+                    line = bufferedReader.readLine();
+
+                    while (line != null) {
+                        strBuilder.append(line);
+                        line = bufferedReader.readLine();
+                    }
+
+                    bufferedReader.close();
+
+                } catch (Exception ex) {
+                    ex.printStackTrace();
+                }
+
+                // parse the json string
+                eqModel.parseJSON(strBuilder.toString());
+            }
+        }
+    }
+}
--- app/src/main/java/com/example/yousafkhan/equake/MainActivity.java
+++ app/src/main/java/com/example/yousafkhan/equake/MainActivity.java
@@ -1,8 +1,10 @@
 package com.example.yousafkhan.equake;
 
+import android.arch.lifecycle.Observer;
+import android.arch.lifecycle.ViewModelProviders;
 import android.content.Intent;
 import android.net.Uri;
-import android.os.AsyncTask;
+import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -10,25 +12,14 @@ import android.widget.AdapterView;
 import android.widget.ListView;
 import android.widget.ProgressBar;
 
-import org.json.JSONArray;
-import org.json.JSONObject;
-
-import java.io.BufferedReader;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.lang.ref.WeakReference;
-import java.net.HttpURLConnection;
-import java.net.URL;
 import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
     private ProgressBar progressBar;
     private ListView listView;
-    private ArrayList<EarthQuake> earthQuakesInfoList;
 
-    private static final String apiURL =
-       "https://earthquake.usgs.gov/fdsnws/event/1/query?format=geojson&starttime=2018-01-01&limit=30";
+    private EarthQuakeVModel earthQuakeViewmodel;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -38,52 +29,17 @@ public class MainActivity extends AppCompatActivity {
         progressBar = findViewById(R.id.progress_bar);
         listView = findViewById(R.id.listview);
 
-        new ApiRequest(this).execute(apiURL);
-        setListViewListener();
-    }
+        earthQuakeViewmodel = ViewModelProviders.of(this).get(EarthQuakeVModel.class);
 
-    private ArrayList<EarthQuake> parseJSON(String jsonString) {
-
-        earthQuakesInfoList = new ArrayList<>();
-
-        double magnitude = 0;
-        String location = "";
-        long time = 0;
-        String detailUrl = "";
-
-        try {
-            // convert JSON string in to JSON object
-            JSONObject jsonObj = new JSONObject(jsonString);
-            // get the features array from the JSON object
-            JSONArray jsonArr = jsonObj.getJSONArray("features");
-
-            // iterate over the features array and extract the required information
-            for (int i=0; i<jsonArr.length(); i++) {
-                // get the JSON object on the current index of the features array
-                // instead of declaring new JSONObject, already declared jsonObj is reused here
-                jsonObj = jsonArr.getJSONObject(i);
-
-                // get the properties object from the current JSON object
-                jsonObj = jsonObj.getJSONObject("properties");
-
-                // get the required information in local variables
-                magnitude = Double.parseDouble(jsonObj.getString("mag"));
-                location = jsonObj.getString("place");
-                time = Long.parseLong(jsonObj.getString("time"));
-                detailUrl = jsonObj.getString("url");
-
-                // create EarthQuake object
-                EarthQuake eq = new EarthQuake(magnitude, location, time, detailUrl);
-
-                // add the newly created EarthQuake object to array list
-                earthQuakesInfoList.add(eq);
+        earthQuakeViewmodel.getDataList().observe(this, new Observer<ArrayList<EarthQuake>>() {
+            @Override
+            public void onChanged(@Nullable ArrayList<EarthQuake> earthQuakes) {
+                progressBar.setVisibility(View.GONE);
+                populateListView(earthQuakes);
             }
+        });
 
-        } catch (Exception ex) {
-            ex.printStackTrace();
-        }
-
-        return earthQuakesInfoList;
+        setListViewListener();
     }
 
     private void populateListView(ArrayList<EarthQuake> list) {
@@ -96,7 +52,7 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
 
-                EarthQuake eq = earthQuakesInfoList.get(position);
+                EarthQuake eq = earthQuakeViewmodel.getItemAtPosition(position);
 
                 // open browser to load the url to view details of the clicked earthquake
                 Intent seeDetails = new Intent(Intent.ACTION_VIEW);
@@ -105,86 +61,4 @@ public class MainActivity extends AppCompatActivity {
             }
         });
     }
-
-    // async task class
-    private static class ApiRequest extends AsyncTask<String, Void, InputStream> {
-
-        private WeakReference<MainActivity> mainActivity;
-
-        ApiRequest(MainActivity context) {
-            mainActivity = new WeakReference<>(context);
-        }
-
-        @Override
-        protected void onPreExecute() {
-            super.onPreExecute();
-        }
-
-        @Override
-        protected InputStream doInBackground(String... strings) {
-
-            String apiURL = strings[0];
-            URL url = null;
-            HttpURLConnection httpConn = null;
-            InputStream inputStream = null;
-
-            try {
-                url = new URL(apiURL);
-                httpConn = (HttpURLConnection) url.openConnection();
-                httpConn.setRequestMethod("GET");
-                inputStream = httpConn.getInputStream();
-
-            } catch (Exception ex) {
-                ex.printStackTrace();
-            } finally {
-                if(httpConn != null) {
-                    httpConn.disconnect();
-                }
-            }
-
-            return inputStream;
-        }
-
-        @Override
-        protected void onProgressUpdate(Void... values) {
-            super.onProgressUpdate(values);
-        }
-
-        @Override
-        protected void onPostExecute(InputStream ins) {
-
-            if(ins != null) {
-
-                String line = "";
-                StringBuilder strBuilder = new StringBuilder();
-
-                InputStreamReader inputStreamReader = new InputStreamReader(ins);
-                BufferedReader bufferedReader = new BufferedReader(inputStreamReader);
-
-                try {
-                    line = bufferedReader.readLine();
-
-                    while (line != null) {
-                        strBuilder.append(line);
-                        line = bufferedReader.readLine();
-                    }
-
-                    bufferedReader.close();
-
-                } catch (Exception ex) {
-                    ex.printStackTrace();
-                }
-
-                // parse the json string
-                ArrayList<EarthQuake> list = mainActivity.get().parseJSON(strBuilder.toString());
-
-                // hide the progress bar
-                mainActivity.get().progressBar.setVisibility(View.GONE);
-
-                // populate the listview
-                mainActivity.get().populateListView(list);
-            }
-        }
-    }
-
 }
